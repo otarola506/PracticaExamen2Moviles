dd81a17ae1db3834ff7e59f4436fa7d48035440a
Sebastian Otarola <otarolasebas@hotmail.com>
2020-11-26 10:01:44 -0600

    Se crea proyecto vacío para implementación de práctica de exámen 2

---
+++ app/src/main/java/cr/ac/ucr/ecci/eseg/miexamen02/MainActivity.java
@@ -0,0 +1,14 @@
+package cr.ac.ucr.ecci.eseg.miexamen02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
