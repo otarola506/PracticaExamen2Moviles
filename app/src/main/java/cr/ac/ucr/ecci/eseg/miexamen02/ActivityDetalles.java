eac87309336ce57f262c65a371fd466d9625e148
Sebastian Otarola <otarolasebas@hotmail.com>
2020-11-27 08:40:27 -0600

    Se crea fragmento de detalles y se implementa código xml para mostrarlos. No se ha juntado con la lista que se recupera del URL porque todavía hay dudas con eso

--- app/src/main/java/cr/ac/ucr/ecci/eseg/miexamen02/ActivityDetalles.java
+++ app/src/main/java/cr/ac/ucr/ecci/eseg/miexamen02/ActivityDetalles.java
@@ -1,6 +1,7 @@
 package cr.ac.ucr.ecci.eseg.miexamen02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,11 @@ public class ActivityDetalles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
+        // Recibo el objeto tableTop
+        TableTop tableTop = getIntent().getExtras().getParcelable("tableTop");
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Inicio el fragmento de detalles con el objeto embajada
+        ft.replace(R.id.fragment_placeholder,new DetallesFragment(tableTop));
+        ft.commit();
     }
 }
---
+++ app/src/main/java/cr/ac/ucr/ecci/eseg/miexamen02/DetallesFragment.java
@@ -0,0 +1,63 @@
+package cr.ac.ucr.ecci.eseg.miexamen02;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetallesFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_TABLETOP = "tableTop";
+
+    private TableTop juego;
+
+
+    public DetallesFragment() {
+        // Required empty public constructor
+    }
+    public DetallesFragment(TableTop juego){
+        this.juego = juego;
+    }
+
+
+    public static DetallesFragment newInstance(TableTop juego) {
+        DetallesFragment fragment = new DetallesFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_TABLETOP, juego);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            juego = getArguments().getParcelable(ARG_TABLETOP);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View v = inflater.inflate(R.layout.fragment_detalles, container, false);
+        // Inflate the layout for this fragment
+        if(juego != null){
+            TextView textIdentificacion = (TextView) v.findViewById(R.id.textID);
+            textIdentificacion.setText(juego.getIdentificacion());
+            TextView textNombre = (TextView) v.findViewById(R.id.textNombre);
+            textNombre.setText(juego.getNombre());
+            TextView textYear = (TextView) v.findViewById(R.id.textAnio);
+            textYear.setText(juego.getYear());
+            TextView textPublisher = (TextView) v.findViewById(R.id.textPublisher);
+            textPublisher.setText(juego.getPublisher());
+        }
+        return v;
+
+    }
+}
