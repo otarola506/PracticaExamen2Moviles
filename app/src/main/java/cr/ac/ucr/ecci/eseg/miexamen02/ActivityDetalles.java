91d28673ff9afbe9a6d36248e51bf697770b64b0
Sebastian Otarola <otarolasebas@hotmail.com>
2020-11-27 08:03:49 -0600

    Se crea actividad de detalles que va a contener el fragmento de detalles.

---
+++ app/src/main/java/cr/ac/ucr/ecci/eseg/miexamen02/ActivityDetalles.java
@@ -0,0 +1,14 @@
+package cr.ac.ucr.ecci.eseg.miexamen02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDetalles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+    }
+}
