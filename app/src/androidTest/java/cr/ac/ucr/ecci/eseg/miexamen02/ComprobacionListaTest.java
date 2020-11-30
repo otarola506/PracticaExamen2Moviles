3c3c27f12b8d9b14e10747c6ab690d02ee70cb88
Sebastian Otarola <otarolasebas@hotmail.com>
2020-11-30 14:15:48 -0600

    Se crea primera prueba instrumentada al app, esta verifica que el juego Pictionary existe y que el detalle sea el correcto

--- app/src/androidTest/java/cr/ac/ucr/ecci/eseg/miexamen02/ComprobacionListaTest.java
+++ app/src/androidTest/java/cr/ac/ucr/ecci/eseg/miexamen02/ComprobacionListaTest.java
@@ -18,15 +18,22 @@ import static org.junit.Assert.assertEquals;
 
 public class ComprobacionListaTest {
 
-    public static final String TEST_STRING_TITULO="Catan";
+    public static final String TEST_ID = "TN008";
+    public static final String TEST_NOMBRE="Pictionary";
+    public static final String TEST_ANIO = "1985";
+    public static final String TEST_PUBLISHER = "Hasbro";
+
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule(MainActivity.class);
     // Prueba que verifica que el juego Catan este en la lista y que el detalle sea el correcto
     @Test
-    public void comprobarCatanExiste() throws InterruptedException {
+    public void comprobarPictionaryExiste() throws InterruptedException {
         Thread.sleep(5000);
-        onView(withText(TEST_STRING_TITULO)).perform(click());
-        onView(withId(R.id.textNombre)).check(matches(withText(TEST_STRING_TITULO)));
+        onView(withText(TEST_NOMBRE)).perform(click());
+        onView(withId(R.id.textID)).check(matches(withText(TEST_ID)));
+        onView(withId(R.id.textNombre)).check(matches(withText(TEST_NOMBRE)));
+        onView(withId(R.id.textAnio)).check(matches(withText(TEST_ANIO)));
+        onView(withId(R.id.textPublisher)).check(matches(withText(TEST_PUBLISHER)));
     }
 }
